cd6e4d00f6581a056d853f04b3571a0ea8d4c45f
alxn787 <alenabraham787@gmail.com>
2025-03-21 20:50:58 +0530

    ui

--- app/exercise/page.tsx
+++ app/exercise/page.tsx
@@ -8,6 +8,7 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-background flex flex-col">
       <main className="flex-1">
+        <div className='h-16'></div>
         <WorkoutTracker />
       </main>
     </div>
