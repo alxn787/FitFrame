1ac70c6a221dd42d545d23fd59ba00e28a72ebeb
alxn787 <alenabraham787@gmail.com>
2025-03-22 16:01:37 +0530

    xdi

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -41,18 +41,19 @@ export default function Navbar() {
                 </div>
                 
                 {/* Navigation Options - Visible on all devices */}
-                <div className="flex flex-wrap justify-center w-full mt-4 md:mt-0 md:justify-center md:ml-8 lg:ml-16">
-                    <button onClick={() => router.push('/dashboard')} className="mx-2 my-1 text-sm lg:text-base text-white text-gray-200 hover:text-gray-400">Dashboard</button>
-                    <button onClick={() => router.push('/guide')} className="mx-2 my-1 text-sm lg:text-base text-white text-gray-200 hover:text-gray-400">Guide</button>
-                    <button onClick={() => router.push('/diet')} className="mx-2 my-1 text-sm lg:text-base text-white text-gray-200 hover:text-gray-400">Diet Plans</button>
-                    <button onClick={() => router.push('/exercise')} className="mx-2 my-1 text-sm lg:text-base text-white text-gray-200 hover:text-gray-400">Exercise</button>
-                    <button onClick={() => router.push('/diet')} className="mx-2 my-1 text-sm lg:text-base text-white text-gray-200 hover:text-gray-400">Pricing</button>
+                <div className="flex flex-wrap justify-center w-full mt-4 md:mt-0 md:justify-center md:ml-8 ">
+                    <button onClick={() => router.push('/dashboard')} className="mx-2 my-1 text-sm lg:text-base text-whitehover:text-gray-400">Dashboard</button>
+                    <button onClick={() => router.push('/guide')} className="mx-2 my-1 text-sm lg:text-base text-white hover:text-gray-400">Guide</button>
+                    <button onClick={() => router.push('/diet')} className="mx-2 my-1 text-sm lg:text-base text-white
+                     hover:text-gray-400">Diet Plans</button>
+                    <button onClick={() => router.push('/exercise')} className="mx-2 my-1 text-sm lg:text-base text-white hover:text-gray-400">Exercise</button>
+                    <button onClick={() => router.push('/diet')} className="mx-2 my-1 text-sm lg:text-base text-white hover:text-gray-400">Pricing</button>
                 </div>
                 
                 {/* Sign In/Out Button for desktop */}
                 <div className="hidden md:flex md:ml-auto">
                     {session?.user ? (
-                        <button className="px-4 py-1.5 lg:px-6 lg:py-2 text-xs lg:text-sm text-white font-semibold bg-[#1a1a1a] rounded-full border border-[#333] shadow-black/50 hover:bg-[#222] transition-all shadow-inner-xl" onClick={() => signOut()}>Sign out</button>
+                        <button className="px-4 py-1.5 lg:px-6 lg:py-2 text-xs  text-white font-semibold bg-[#1a1a1a] rounded-full border border-[#333] shadow-black/50 hover:bg-[#222] transition-all shadow-inner-xl" onClick={() => signOut()}>Sign out</button>
                     ) : (
                         <button className="px-4 py-1.5 lg:px-6 lg:py-2 text-xs lg:text-sm text-white font-semibold bg-[#1a1a1a] rounded-full border border-[#333] shadow-md shadow-black/50 hover:bg-[#222] transition-all" onClick={() => signIn("google")}>Sign in</button>
                     )}
